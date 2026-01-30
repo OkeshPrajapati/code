3788e7e5e40cbfba941469231213624c6d85b5c0
Okesh Prajapati <okeshprajapati920@gmail.com>
2026-01-30 18:41:43 +0530

    chg db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    let res = await mongoose.connect("mongodb://okeshprajapati920_db_user:v49rIHgfEsjDYBds@crud.b7zskmf.mongodb.net/day-1");
+    let res = await mongoose.connect("mongodb+srv://okeshprajapati920_db_user:v49rIHgfEsjDYBds@crud.b7zskmf.mongodb.net/day-1");
     // let res = await mongoose.connect("mongodb://localhost:27017/day-1");
     if (res) {
       console.log("mongodb Connected");
